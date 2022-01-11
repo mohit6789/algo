f2875adbea33a7c587e2e1d6faa8ad445890f8a7
mohit <mohit_raichura20000@yahoo.com>
2022-01-11 05:45:43 +0000

    Generic runner to read the json file and execute all the test cases

---
+++ runner.js
@@ -0,0 +1,25 @@
+const assert = require('assert').strict;
+const fs = require('fs')
+
+const runner = (fn) => {
+  console.log(process.argv);
+  if(process.argv.length !== 3) {
+    throw new Error("Please pass JSON filename");
+  }
+  const fileName = process.argv[2];
+  try {
+    const fileData = fs.readFileSync(`./inputs/${fileName}.json`, 'utf8');
+    let data = JSON.parse(fileData);
+    data.forEach(({inputs, output}) => {
+      const result = fn(...inputs);
+      assert.deepEqual(result, output, `Fail for "${inputs}" => expected "${result}" actual "${output}"`);
+    });
+    console.log("Executed successfully.");
+  } catch (e) {
+    console.error("Error when reading inputs", e);
+  }
+}
+
+module.exports = {
+  runner
+}
